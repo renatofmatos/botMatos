faa2f35227bc4ada92656ff0d1f7e5ba1d6e87bc
renatofmatos <rfarias.matos@gmail.com>
2025-02-28 14:49:42 -0300

    teste vercel

--- dist/controllers/mensagemController.js
+++ dist/controllers/mensagemController.js
@@ -1,10 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const apikey = process.env.API_KEY;
 class Mensagem {
     static async criarMensagem(req, res) {
         try {
-            console.log('Mensagem recebida: ', req.body);
-            res.status(200).send('Mensagem recebida');
+            if (req.query['hub.mode'] === 'subscribe' &&
+                req.query['hub.verify_token'] === apikey) {
+                console.log('APIKEY autorizada');
+                res.send(req.query['hub.challenge']);
+            }
+            else {
+                console.log('Mensagem recebida:', req.body);
+                res.sendStatus(200);
+            }
         }
         catch (error) {
             res.status(500).json({ message: `Falha na requisição: ${error}` });
--- dist/server.js
+++ dist/server.js
@@ -25,4 +25,5 @@ async function iniciarServidor() {
 }
 ;
 iniciarServidor();
+exports.default = iniciarServidor;
 //# sourceMappingURL=server.js.map
