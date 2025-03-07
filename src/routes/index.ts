0fa7a4cd8016820828fc9dfe1df76477cc2d059f
renatofmatos <rfarias.matos@gmail.com>
2025-03-06 22:06:48 -0300

    Ajuste Primeiro teste de webhook

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,13 @@ import { json, Application, Request, Response } from "express";
 import meiosDecontato from './meiosDeContatoRoutes.js';
 import clientes from './clientesRoutes.js';
 import mensagens from "./mensagemRoutes.js";
+import webhook from "./webhookRoutes.js";
 
 const routes = (app: Application): void => {
     app.route('/').get((req: Request, res: Response) => {
         res.status(200).send('Matos Tecnologia');
     });
-    app.use(json(), clientes, meiosDecontato, mensagens);
+    app.use(json(), clientes, meiosDecontato, mensagens, webhook);
 };
 
 export default routes;
