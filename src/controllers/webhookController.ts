67094b364c99d9f86b132d17f660e5ea49d3b753
renatofmatos <rfarias.matos@gmail.com>
2025-03-14 11:01:04 -0300

    Não processar requisição de status

--- src/controllers/webhookController.ts
+++ src/controllers/webhookController.ts
@@ -25,25 +25,29 @@ class WebhookController {
     static async mensagemRecebida(req: Request, res: Response) {
         console.log("Mensagem recebida!:", JSON.stringify(req.body, null, 2));
         const message = await req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
-        const dataRecebimentoMensagem: Date = new Date(Number(await message.timestamp) * 1000);
-        const corpoMensagem: string = await message.text.body;
-        const remetenteId: string = await message.from;
-        const tipoConteudoMensagem: string = await message.type;
-        const mensagemIdSistemaOrigem: string = await message.id;
-        const destinatarioId: string = await req.body.entry?.[0]?.changes[0]?.value?.metadata?.display_phone_number;
-        const nomeContato: string = await req.body.entry?.[0]?.changes[0]?.value?.contacts[0]?.profile.name;
+        if (message) {
+            const dataRecebimentoMensagem: Date = new Date(Number(await message.timestamp) * 1000);
+            const corpoMensagem: string = await message.text.body ?? message.button.text;
+            const remetenteId: string = await message.from;
+            const tipoConteudoMensagem: string = await message.type;
+            const mensagemIdSistemaOrigem: string = await message.id;
+            const destinatarioId: string = await req.body.entry?.[0]?.changes[0]?.value?.metadata?.display_phone_number;
+            const nomeContato: string = await req.body.entry?.[0]?.changes[0]?.value?.contacts[0]?.profile.name;
 
-        const atendimentoProcessado = AtendimentoService.processarMensagem(
-            remetenteId,
-            destinatarioId,
-            nomeContato,
-            dataRecebimentoMensagem,
-            corpoMensagem,
-            tipoConteudoMensagem,
-            mensagemIdSistemaOrigem
-        );
+            const atendimentoProcessado = AtendimentoService.processarMensagem(
+                remetenteId,
+                destinatarioId,
+                nomeContato,
+                dataRecebimentoMensagem,
+                corpoMensagem,
+                tipoConteudoMensagem,
+                mensagemIdSistemaOrigem
+            );
 
-        res.sendStatus(200);
+            res.sendStatus(200);
+        } else {
+            console.log(`Mensagem de status recebida e não processada!`);
+        }
     };
 };
 
