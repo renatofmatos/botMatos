86985297f29c5b7b6f739856d6f7951ce3a941b3
renatofmatos <rfarias.matos@gmail.com>
2025-03-14 09:56:53 -0300

    teste nome do cliente

--- src/services/mensagemService.ts
+++ src/services/mensagemService.ts
@@ -4,7 +4,11 @@ import MensagemController from "../controllers/mensagemController.js";
 export class MensagemService {
     static responderMensagem(mensagem: Mensagem, nomeCliente?: string) {
         MensagemModel.create(mensagem);
-        MensagemController.responderMensagem(mensagem, nomeCliente);
+        if (nomeCliente) {
+            MensagemController.responderMensagem(mensagem, nomeCliente);
+        }else {
+            console.log(`Nome do cliente indefinido`);
+        }
     }
 
     static async marcarMensagemLida(mensagemIdSistemaOrigem: string) {
